f977ef5f425a8c774cff18de921d5413160943b5
John Sanabria <john.sanabria@correounivalle.edu.co>
2021-04-16 00:52:49 +0000

    listo para la tarea

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ docker build -t baseflask .
 ```
 docker run --rm -it -p 5001:5000 -v $(pwd):/workdir -d baseflask
 ```
+
+## Como consumir los web services
+
+Acceder al metodo `A1` del programa `demo-gspread.py`
+
+```
+curl -X GET -H "Content-type: application/json" -d '{"saludo": "gutten neicht" }' http://localhost:5001/
+```
+
+Acceder al metodo `obtenervalor` del programa `demo-gspread.py`
+
+```
+curl -X POST -H "Content-type: application/json" -d '{"row": "3", "col": "2" }' http://localhost:5001/obtenervalor
+```
+
+
--- demo-gspread.py
+++ demo-gspread.py
@@ -1,5 +1,5 @@
 import gspread
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 gc = gspread.service_account(filename="/workdir/demo-ls.json")
@@ -7,7 +7,14 @@ sh = gc.open("DEMO-LS")
 
 @app.route("/")
 def A1():
+  print(request.json['saludo'])
   return sh.sheet1.cell(1,1).value
+
+@app.route("/obtenervalor",methods=['POST'])
+def obtenervalor():
+  row = request.json['row']
+  col = request.json['col']
+  return sh.sheet1.cell(row, col).value
 
 if  __name__ == '__main__':
   app.run(host='0.0.0.0')
